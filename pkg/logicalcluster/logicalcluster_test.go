4cb6e8d91866666be5bd5789e4f7c3f6048c99ca
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-03-19 23:46:44 +0100

    logicalcluster: add tests for Split

---
+++ pkg/logicalcluster/logicalcluster_test.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2022 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package logicalcluster
+
+import (
+	"testing"
+)
+
+func TestLogicalCluster_Split(t *testing.T) {
+	tests := []struct {
+		cn     LogicalCluster
+		parent LogicalCluster
+		name   string
+	}{
+		{"", "", ""},
+		{"foo", "", "foo"},
+		{"foo:bar", "foo", "bar"},
+		{"foo:bar:baz", "foo:bar", "baz"},
+		{"foo::baz", "foo:", "baz"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotParent, gotName := tt.cn.Split()
+			if gotParent != tt.parent {
+				t.Errorf("Split() gotParent = %v, want %v", gotParent, tt.parent)
+			}
+			if gotName != tt.name {
+				t.Errorf("Split() gotName = %v, want %v", gotName, tt.name)
+			}
+		})
+	}
+}
