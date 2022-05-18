f62703561e55f13cac22babf71900ca1e34e7107
Fabian von Feilitzsch <fabian@fabianism.us>
2022-05-18 11:25:49 -0400

    Add discovery client (#22)
    Add a logical cluster aware discovery client

---
+++ pkg/discovery/discovery_client.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2022 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package discovery
+
+import (
+	"time"
+
+	"github.com/kcp-dev/logicalcluster"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/kubernetes/scheme"
+	restclient "k8s.io/client-go/rest"
+)
+
+const (
+	// defaultTimeout is the maximum amount of time per request when no timeout has been set on a RESTClient.
+	// Defaults to 32s in order to have a distinguishable length of time, relative to other timeouts that exist.
+	defaultTimeout = 32 * time.Second
+)
+
+// ClusterDiscoveryClient holds an unscoped REST config and client
+type ClusterDiscoveryClient struct {
+	restClient *restclient.RESTClient
+	config     *restclient.Config
+}
+
+// NewClusterDiscoveryClientForConfig takes a rest config and returns a ClusterDiscoveryClient
+// that can create scoped discovery clients for logical clusters
+func NewClusterDiscoveryClientForConfig(c *restclient.Config) (*ClusterDiscoveryClient, error) {
+	err := setDiscoveryDefaults(c)
+	if err != nil {
+		return nil, err
+	}
+
+	httpClient, err := restclient.HTTPClientFor(c)
+	if err != nil {
+		return nil, err
+	}
+	client, err := restclient.UnversionedRESTClientForConfigAndClient(c, httpClient)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ClusterDiscoveryClient{config: c, restClient: client}, nil
+}
+
+// Cluster creates a discovery client scoped to the specified logical cluster
+func (c *ClusterDiscoveryClient) Cluster(cluster logicalcluster.Name) discovery.DiscoveryInterface {
+	scopedConfig := restclient.CopyConfig(c.config)
+
+	scopedConfig.Host = scopedConfig.Host + "/" + cluster.Path()
+
+	// The original rest config has already parsed as valid. Modifying the host to scope it to a valid
+	// cluster should never result in an invalid URL, so there should be no real possibility of a panic here.
+	return discovery.NewDiscoveryClientForConfigOrDie(scopedConfig)
+}
+
+// setDiscoveryDefaults sets sane defaults in the provided REST config to allow API discovery to succeed
+// Original source:
+// https://github.com/kubernetes/kubernetes/blob/d126b1483840b5ea7c0891d3e7a693bd50fae7f8/staging/src/k8s.io/client-go/discovery/discovery_client.go#L463
+func setDiscoveryDefaults(config *restclient.Config) error {
+	config.APIPath = ""
+	config.GroupVersion = nil
+	if config.Timeout == 0 {
+		config.Timeout = defaultTimeout
+	}
+	if config.Burst == 0 && config.QPS < 100 {
+		// discovery is expected to be bursty, increase the default burst
+		// to accommodate looking up resource info for many API groups.
+		// matches burst set by ConfigFlags#ToDiscoveryClient().
+		// see https://issue.k8s.io/86149
+		config.Burst = 100
+	}
+	codec := runtime.NoopEncoder{Decoder: scheme.Codecs.UniversalDecoder()}
+	config.NegotiatedSerializer = serializer.NegotiatedSerializerWrapper(runtime.SerializerInfo{Serializer: codec})
+	if len(config.UserAgent) == 0 {
+		config.UserAgent = restclient.DefaultKubernetesUserAgent()
+	}
+	return nil
+}
---
+++ pkg/discovery/discovery_client_test.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2022 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package discovery
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/kcp-dev/logicalcluster"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+)
+
+func TestClusterDiscoveryClient(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		var obj interface{}
+		switch req.URL.Path {
+		case "/clusters/cluster1/api":
+			obj = &metav1.APIVersions{
+				Versions: []string{
+					"v1",
+				},
+			}
+		case "/clusters/cluster1/apis":
+			obj = &metav1.APIGroupList{
+				Groups: []metav1.APIGroup{
+					{
+						Name: "extensions",
+						Versions: []metav1.GroupVersionForDiscovery{
+							{GroupVersion: "extensions/v1beta1"},
+						},
+					},
+				},
+			}
+		case "/clusters/cluster2/api":
+			obj = &metav1.APIVersions{
+				Versions: []string{
+					"v2",
+				},
+			}
+		case "/clusters/cluster2/apis":
+			obj = &metav1.APIGroupList{
+				Groups: []metav1.APIGroup{
+					{
+						Name: "extensions",
+						Versions: []metav1.GroupVersionForDiscovery{
+							{GroupVersion: "extensions/v2beta2"},
+						},
+					},
+				},
+			}
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		output, err := json.Marshal(obj)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(output)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}))
+
+	defer server.Close()
+
+	client, err := NewClusterDiscoveryClientForConfig(&rest.Config{Host: server.URL + "/"})
+	require.NoError(t, err)
+
+	cluster1Client := client.Cluster(logicalcluster.New("cluster1"))
+	apiGroupList, err := cluster1Client.ServerGroups()
+	require.NoError(t, err)
+
+	groupVersions := metav1.ExtractGroupVersions(apiGroupList)
+	require.EqualValues(t, groupVersions, []string{"v1", "extensions/v1beta1"})
+
+	cluster2Client := client.Cluster(logicalcluster.New("cluster2"))
+	apiGroupList, err = cluster2Client.ServerGroups()
+	require.NoError(t, err)
+
+	groupVersions = metav1.ExtractGroupVersions(apiGroupList)
+	require.EqualValues(t, groupVersions, []string{"v2", "extensions/v2beta2"})
+}
